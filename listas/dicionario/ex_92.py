4398d523f7af783ecacf47f4b1de84a00266f26f
Breno Morais won Muhlen <brenomwm@gmail.com>
2025-12-04 16:41:14 -0400

    Update ex_92.py

---
+++ listas/dicionario/ex_92.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia nome, ano de nascimento, idade e carteira de trabalho e cadastre-o em um dicionário.
+# Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
+# Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+
+pessoa = {'nome' : 'breno'}
