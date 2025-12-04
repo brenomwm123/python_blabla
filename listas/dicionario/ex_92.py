760fb9fad7af45b8876bf33cc9c6db56dbe2d472
Breno Morais won Muhlen <brenomwm@gmail.com>
2025-12-04 16:48:03 -0400

    Update ex_92.py

--- listas/dicionario/ex_92.py
+++ listas/dicionario/ex_92.py
@@ -2,4 +2,14 @@
 # Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
 # Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 
-pessoa = {'nome' : 'breno'}
+from datetime import datetime
+
+ano_atual = datetime.now().year
+
+pessoa = {}
+pessoa['nome'] = str(input('Nome: '))
+pessoa['nascimento'] = int(input('Ano de nascimento: '))
+pessoa['c_trabalho'] = str(input('Numero da carteira de trabalho, 0 caso nao tenha: '))
+pessoa['idade'] = ano_atual - pessoa['nascimento']
+
+print(pessoa['idade'])
