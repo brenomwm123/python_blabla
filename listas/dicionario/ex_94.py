8f9cf7f69200b72ef5e4e94159a8e6e13721a60a
Breno Morais won Muhlen <brenomwm@gmail.com>
2025-12-08 15:40:52 -0400

    Update ex_94.py

--- listas/dicionario/ex_94.py
+++ listas/dicionario/ex_94.py
@@ -6,16 +6,29 @@
 # C) Uma lista com as mulheres
 # D) Uma lista de pessoas com idade acima da média
 
-pessoas = {}
+
+pessoas = []
 n_pessoas = 0
+iade_media = 0
 
 while True:
-    pessoas['nome'] = str(input('Nome: '))
-    pessoas['sexo'] = str(input('Sexo F/M : '))
-    pessoas['idade'] = int(input('Idade: '))
+    pessoa = {}
+    pessoa['nome'] = str(input('Nome: '))
+    
+    while True:
+        pessoa['sexo'] = str(input('Sexo F/M : ')).upper().strip()
+        if pessoa['sexo'] in ['F', 'M']:
+            break
+        else:
+            print('errado.')
+            
+    pessoa['idade'] = int(input('Idade: '))
     n_pessoas += 1
+    pessoas.append(pessoa)
     resp = input('Deseja inserir outra pessoa ? S/N :')
     if resp in 'Nn':
         break
-    
-print(pessoas)
+
+    print(f'Foram cadastradas {n_pessoas} pessoas.')
+
+    #for k, v in 
