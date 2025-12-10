864bea09b82474991092723afdd2a63c2bc0a981
Breno Morais won Muhlen <brenomwm@gmail.com>
2025-12-10 16:29:44 -0400

    Update ex_95.py

--- listas/dicionario/ex_95.py
+++ listas/dicionario/ex_95.py
@@ -25,13 +25,18 @@ print('-='*25)
 print(f'{"cod":<4}{"nome":<15}{"gols":<20}{"total":>5}')
 print('-' * 50)
 for i, j in enumerate(jogadores):
-    print(f'{i:<4}{j["nome"]:<15}{str(j["gols"]):<20}{sum(j["gols"]):>5}')
-# print(f'O jogador {jogador["nome"]} jogou {jogador["partidas"]} partidas')
-
-# print('-='*20)
-
-# for i in range(jogador['partidas']):
-#     print(f'Na partida {i+1}, {jogador["nome"]} marcou {jogador["gols"][i]} gols.')
-# print('-='*20)
-
-# print(f'Total de gols: {gols_tot}')
+    print(f'{i+1:<4}{j["nome"]:<15}{str(j["gols"]):<20}{sum(j["gols"]):>5}')
+    
+while True:
+    print('-' * 50)
+    cod = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    
+    if cod == 999:
+        print('Encerrado')
+        break
+    if cod >= len(jogadores):
+        print('Codigo invalido ! Tente novamente.')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {jogadores[cod]["nome"]}:')
+        for i, g in enumerate(jogadores[cod+1]["gols"]):
+            print(f'   No jogo {i+1} fez {g} gols.')
