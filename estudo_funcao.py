16ed06a0d40cc0a6ba06657cd17ef52da97d9dd3
Breno Morais won Muhlen <brenomwm@gmail.com>
2025-12-15 16:25:29 -0400

    ex

---
+++ estudo_funcao.py
@@ -0,0 +1,13 @@
+def double(list):
+    pos = 0
+    while pos<len(list):
+        list[pos] *= 2
+        pos += 1
+
+valores = [1, 4, 7, 2, 5, 6, 70]
+
+double(valores)
+print(valores)
+
+
+    
---
+++ funcao/ex_96.py
@@ -0,0 +1,15 @@
+
+def bloco(texto):
+    print('-='*20)
+    print(f'{texto:^40}')
+    print('-='*20)
+#def área(l, c):
+    
+bloco('Programa medidor de terreno')
+
+l = float(input('Informe a largura do terreno: '))
+c = float(input('Informe o comprimento do terreno: '))
+
+
+
+    
