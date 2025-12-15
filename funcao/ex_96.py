613623ad08fd02d78fc12a16c0d7e968de9bb553
Breno Morais won Muhlen <brenomwm@gmail.com>
2025-12-15 16:59:35 -0400

    Update ex_96.py

--- funcao/ex_96.py
+++ funcao/ex_96.py
@@ -3,13 +3,21 @@ def bloco(texto):
     print('-='*20)
     print(f'{texto:^40}')
     print('-='*20)
-#def área(l, c):
     
+    
+def área(l, c):
+    area = l * c
+    print(f'A area de um terreno de {l} por {c} é de {area:.2f}m2.')
+
+
+#Codigo principal
+
 bloco('Programa medidor de terreno')
 
 l = float(input('Informe a largura do terreno: '))
 c = float(input('Informe o comprimento do terreno: '))
 
+área(l, c)
 
 
     
