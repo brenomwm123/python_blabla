1bb6f76c730c045d9457ccdad4c8c19da3aac90e
Breno Morais won Muhlen <brenomwm@gmail.com>
2025-12-16 16:12:21 -0400

    Update ex_98.py

--- funcao/ex_98.py
+++ funcao/ex_98.py
@@ -1,21 +1,24 @@
 from time import sleep
 
+def linha():
+    print('=-'*20)
+    
 def contador():
     nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    print('=-'*20)
+    linha()
     print('Contagem de 1 até 10 de 1 em 1')
     for num in nums[1:]:
         print(num, end=' ', flush = True)
         sleep(0.4)
-    print()
-    print('=-'*20)
+    print(':p')
+    linha()
     print('Contagem de 10 até 0 de 2 em 2')
     sleep(1.8)
     for num in nums[::-2]:
         print(num, end=' ', flush = True)
         sleep(0.4)
-    print()
-    print('=-'*20)
+    print(':p')
+    linha()
         
 
 contador() 
