f16fadca9090f301ac4647a55c007e5b2eab8a67
Breno Morais won Muhlen <brenomwm@gmail.com>
2026-01-06 08:13:51 -0400

    Update ex_105.py

--- funcao/ex_105.py
+++ funcao/ex_105.py
@@ -3,10 +3,24 @@
 #maior nota, menor nota, media da turma, situacao(opcional)
 
 def notas(*n, sit=False):
-    print(n)
+    r = {}
+    r['total'] = len(n)
+    if len(n) == 0:
+        return{}
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['media'] = round(sum(n)/len(n), 2)
     
+    if sit:
+        if r['media'] >= 7:
+            r['situação'] = 'BOM'
+        elif r['media'] >= 5:
+            r['situação'] = 'RAZOAVEL'
+        else:
+            r['situação'] = 'RUIM'           
+    return r
     
-    
-    
+        
 #Codigo principal
-notas(5.5, 8.7, 9.0, 10.0, 1.5, 0.5, sit=True)
+resposta = notas(5.5, 8.7, 9.0, 10.0, 1.5, 0.5, sit=True)
+print(resposta)
