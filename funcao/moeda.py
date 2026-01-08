5520f43b9d67be1154dd8e55493ae96435706b8c
Breno Morais won Muhlen <brenomwm@gmail.com>
2026-01-08 10:09:26 -0400

    .

---
+++ funcao/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(valor):
+    a = valor * 1.15
+    return a
+
+def diminuir(valor):
+    d = valor * 0.87
+    return d
+
+def dobro(valor):
+    d = valor * 2
+    return d
+
+def metade(valor):
+    m = valor / 2
+    return m
+
