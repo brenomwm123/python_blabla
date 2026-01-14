085f0788d9b2dd48d203232612f278916cd5d7b0
Breno Morais won Muhlen <brenomwm@gmail.com>
2026-01-14 15:14:51 -0400

    .

---
+++ ex_112.py
@@ -0,0 +1,9 @@
+from utilidadesCeV import moeda
+
+def linha():
+    print('-='*15)
+
+#Programa principal
+
+preco = float(input('Informe o preço: '))
+moeda.amostrar(preco, 90, 5)
--- utilidadesCeV/dados.py
+++ utilidadesCeV/dados.py
@@ -1,3 +1,10 @@
 def leiaDinheiro(valor):
-    d = float(input('Informe um valor: '))
+    valido = False
+    while not valido:  
+        entrada = str(input('Informe um valor: '))
+        if entrada.isalpha():
+            print('Preço invalido.')
+        else:
+            valido = True
+            return float(entrada)
     
