4301fd076607a81b861686e6d5b434f8745cb54b
Breno Morais won Muhlen <brenomwm@gmail.com>
2026-01-20 16:41:56 -0400

    Update teste.py

--- ex115/teste.py
+++ ex115/teste.py
@@ -5,26 +5,14 @@ from lib.interface import *
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if resposta not in (1, 2, 3):
-        print('Opção informada invalida\nPor favor digite 1, 2 ou 3')
+        print('\033[31mOpção informada invalida\nPor favor digite 1, 2 ou 3\033[m')
         continue
-    elif resposta == 1:
+    elif resposta == 1:               
+        cabecalho('VOCE SELECIONOU : Ver pessoas cadastradas')
         sleep(1)
-        print('-'*30)
-        print(f'{"VOCE SELECIONOU : Ver pessoas cadastradas":^30}')
-        sleep(2)
-        print('\n')
-    elif resposta == 2:
-            sleep(1)
-            print('-'*30)
-            print(f'{"VOCE SELECIONOU: Cadastrar nova pessoa":^30}')
-            sleep(2)
-            print('\n')
-    elif resposta == 3:
-            sleep(1)
-            print('-'*30)
-            print(f'{"VOCE SELECIONOU: Sair do sistema":^30}')
-            sleep(2)
-            print('Até mais :)')
-            sleep(0.7)
-            print('\n')
-            break               
+    elif resposta == 2:                        
+        cabecalho('"VOCE SELECIONOU: Cadastrar nova pessoa"')
+        sleep(1)
+    elif resposta == 3:           
+        cabecalho('Saindo do sistema')
+        break               
