1c9a7661a4d9870fe7d6cc41e668a1ff41d8310e
Breno Morais won Muhlen <brenomwm@gmail.com>
2026-01-22 16:17:53 -0400

    .

--- ex115/lib/arquivo.py
+++ ex115/lib/arquivo.py
@@ -1,3 +1,5 @@
+from lib.interface import *
+
 def arquivoExiste(nome):
     try:
         a = open(nome, 'rt')
@@ -6,6 +8,21 @@ def arquivoExiste(nome):
         return False
     else:
         return True
-    
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Houve um erro na criação do arquivo')
+    else:
+        print(f'Arquivo {nome} criado com sucesso !')
         
-        
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler arquivo')
+    else:
+        cabecalho('PESSOAS CADASTRADAS')
+        print(a.readlines())
